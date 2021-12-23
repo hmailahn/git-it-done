d8aca10e9f0e22db965d5a5c440ed98f759cbdcf
mailahnheidi@gmail.com <mailahnheidi@gmail.com>
2021-12-23 14:51:12 -0600

    added display limit link

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,3 +1,18 @@
+var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+
+var displayWarning = function(repo) {
+    //add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo +"/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    //apend to warning contianer
+    limitWarningEl.appendChild(linkEl);
+};
+
 var getRepoIssues = function(repo) {
     apiURL = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
@@ -5,7 +20,13 @@ var getRepoIssues = function(repo) {
         //request was successful
         if (response.ok) {
             response.json().then(function(data) {
-                console.log(data);
+                //pass response data to dom function
+                displayIssues(data);
+
+                //check if api has paginated issues
+                if (displayWarning(repo)) {
+                    console.log("repo has more than 30 issues");
+                }
             });
         } 
         else {
@@ -14,4 +35,42 @@ var getRepoIssues = function(repo) {
     });
 };
 
-getRepoIssues("facebook/react");
+var displayIssues = function(issues) {
+if (issues.length === 0) {
+    issueContainerEl.textContent = "This repo has no open issues!";
+    return;
+}
+
+    for (var i = 0; i < issues.length; i++) {
+        //create a link ellemnt to take users to the issue on github
+        var issueEl = document.createElement("a");
+        issueEl.classList = "list-item flex-row jusitfy-space-between align-center";
+        issueEl.setAttribute("href", issues[i].html_url);
+        issueEl.setAttribute("target", "_blank");
+
+        //create span to hold issue title
+        var titleEl = document.createElement("span");
+        titleEl.textContent = issues[i].title;
+
+        //append to container
+        issueEl.appendChild(titleEl);
+
+        //create a type element
+        var typeEl = document.createElement("span");
+
+        //check if issue is an actual issue or pull request
+        if (issues[i].pull_request) {
+            typeEl.textContent = "(Pull request)";
+        } else {
+            typeEl.textContent = "(Issue)";
+        }
+
+        //apend to container
+        issueEl.appendChild(typeEl);
+
+        issueContainerEl.appendChild(issueEl);
+    }
+
+};
+
+getRepoIssues("angular/angular");
